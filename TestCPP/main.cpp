617fdf10f182fb6aee66b399b6b6d08eb5eddca6
TeeRatz <triratz@gmx.de>
2014-12-21 13:55:17 +0100

    Bingo

--- TestCPP/main.cpp
+++ TestCPP/main.cpp
@@ -11,6 +11,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    printf("Yeah this is great and it is working" + "Did everything work?");
+    printf("Yeah this is great and it is working. Did everything work? \n");
     return 0;
 }
