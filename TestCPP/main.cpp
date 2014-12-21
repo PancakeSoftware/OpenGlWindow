6c9d8c7e375790a1e81cd78b8f934c82810a549c
TeeRatz <triratz@gmx.de>
2014-12-21 16:25:06 +0100

    Will it work?

---
+++ TestCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestCPP
+//
+//  Created by Tristan Ratz on 21.12.14.
+//  Copyright (c) 2014 Pancake Software. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
