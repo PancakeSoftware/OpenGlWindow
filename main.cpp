96fd838fec0486484ba998ef26cd1ccddd12e79f
JeyRunner <joshua@johannson.org>
2014-12-22 14:22:11 +0100

    setup basic functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,66 @@
-// include
+/* -- INCLUDE ------------------------------------------------ */
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
 
-// the main programm
+/* @TODO
+ * different includes for different platforms
+ */
+
+
+
+
+/* -- MAIN PROGRAM ------------------------------------------- */
 int main()
 {
-    // some output
-    for (int i; i<1000; i++)
-    {
-        cout << "Hello, World; Count: " << i << endl;
-    }
+    // output
+    cout << "[ OK ] program start" << endl;
+    
+        
+}
+
+
+
+/* -- CREATE OPENGL WINDOW ------------------------------------ */
+// -- Linux - Ubuntu 
+bool createWindowUbuntu()
+{
+    cout << "[....] creating Window for Ubuntu" << endl;
+    
+    /* @TODO
+     * create openGl Surface for Ubuntu
+     */
+}
+
+
+// -- Linux - Pi
+bool createWindowPi()
+{
+    cout << "[....] creating Window for Raspberry pi" << endl;
+    
+    /* @TODO
+     * create openGl Surface for Pi
+     */
+}
+
+
+// -- Mac
+bool createWindowMac()
+{
+    cout << "[....] creating Window for Mac" << endl;
+    
+    /* @TODO
+     * create openGl Surface for Mac OSx
+     */
+}
+
+
+// -- Windows
+bool createWindowWin()
+{
+    cout << "[....] creating Window for Windows" << endl;
+    
+    /* @TODO
+     * create openGl Surface for Windows
+     */
 }
